a45893cd7f9856a8e3c7c07ed69b3d0bce831178
Kate Huddleston <kdavis2@ufl.edu>
2023-06-22 18:16:55 -0400

    Updated template files to incorporate config file

--- templates/template_model_loader.py
+++ templates/template_model_loader.py
@@ -1,25 +1,6 @@
-from personal_trainer import evaluator
-import torch
-import torchani
-
-pe = evaluator.personal_evaluator(device = torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
-            wkdir = '/data/khuddzu/GSAEs/training_center/logs/1x/20211013_0051_ani1xGSAE_biasfalse_network',
-            checkpoint = 'best',
-            netlike1x = True,
-            netlike2x = False,
-            functional = 'wb97x',
-            basis_set = '631gd',
-            constants = None,
-            elements = ['H', 'C', 'N', 'O', 'S', 'F', 'Cl'],
-            gsae_dat = None,
-            activation = torch.nn.GELU(),
-            bias = False,
-            classifier_out = 1,
-            personal = False)
-
-
-aev_computer, energy_shifter, model = pe.model_builder()
-
-print(aev_computer)
-print(energy_shifter)
+import personal_trainer
+from personal_trainer import protocol
+pe = protocol.personal_trainer('editor.ini')
+ac, es, model = pe.model_loader(wkdir = '/data/khuddzu/networks/ANI-2x_mbis/training_center/logs/20230620_1231_mbis_ANI2x_coulomb_CE_mtl_trial4/',
+                                checkpoint = 'best.pt')
 print(model)
