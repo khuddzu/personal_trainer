b42555fa4c663e378f730a29bac44afc3aeee615
Kate Huddleston <kdavis2@ufl.edu>
2023-06-24 11:29:24 -0400

    updated templates to include charge types

--- templates/template_model_loader.py
+++ templates/template_model_loader.py
@@ -1,5 +1,7 @@
 import personal_trainer
 from personal_trainer import protocol
+
+#wkdir = 'PATH TO MODEL AND DIRECTORY WITH CHECKPOINT FILE'
 pe = protocol.personal_trainer('editor.ini')
 ac, es, model = pe.model_loader(wkdir = '/data/khuddzu/networks/ANI-2x_mbis/training_center/logs/20230620_1231_mbis_ANI2x_coulomb_CE_mtl_trial4/',
                                 checkpoint = 'best.pt')
