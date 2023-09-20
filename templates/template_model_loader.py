38d1f5229a569068b8a104351d0bda8ebd421535
Kate Huddleston <kdavis2@ufl.edu>
2023-09-20 11:40:26 -0400

    Updated template model loader to current model loader function and added device

--- templates/template_model_loader.py
+++ templates/template_model_loader.py
@@ -1,8 +1,12 @@
 import personal_trainer
 from personal_trainer import protocol
+import torch
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 #wkdir = 'PATH TO MODEL AND DIRECTORY WITH CHECKPOINT FILE'
-pe = protocol.personal_trainer('editor.ini')
-ac, es, model = pe.model_loader(wkdir = '/data/khuddzu/networks/ANI-2x_mbis/training_center/logs/20230620_1231_mbis_ANI2x_coulomb_CE_mtl_trial4/',
-                                checkpoint = 'best.pt')
+wkdir = '/data/khuddzu/networks/ANI-2x_mbis/training_center/logs/20230821_1223_mbis_ANI2x_screened_-4_2_mtl_trial1/'
+pe = protocol.personal_trainer(f'{wkdir}/editor.ini', device=device)
+aev_computer, self_energies, model, nn = pe.model_loader(wkdir=wkdir,
+                                                        checkpoint = 'best.pt')
 print(model)
