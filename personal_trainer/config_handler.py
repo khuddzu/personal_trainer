ba39d4cce48beb6768934b6e64bdbf7c019c843e
Kate Huddleston <kdavis2@ufl.edu>
2024-06-24 15:36:52 -0400

    Modify config_handler to better support specifications of model and edit tests

--- personal_trainer/config_handler.py
+++ personal_trainer/config_handler.py
@@ -1,5 +1,7 @@
 import configparser
-
+import torchani
+import os
+import importlib
 
 class ConfigHandler:
     def __init__(self, config_file):
@@ -34,7 +36,7 @@ class ConfigHandler:
         """
         return eval(self.global_vars.get("device"))
 
-    def get_value(self, section, key):
+    def _convert(self, section, key):
         """
         Retrieve the value of a key in a given section and convert it to the appropriate type.
 
@@ -50,29 +52,45 @@ class ConfigHandler:
         any
             The value of the key, converted to the appropriate type.
 
-        Notes
-        -----
-        Activation in global section might be depricated.
         """
         value = self.config[section].get(key)
         try:
             return eval(value)  # Try to evaluate the value
         except BaseException:
             return value  # Return as string if evaluation fails
-
-    def get_all_values(self):
+    
+    def load_config(self):
         """
         Retrieve all configuration values, organized by section.
+        AEV param files and elements are also established based on the preference of like1x or like2x. 
 
         Returns
         -------
         dict
             A dictionary containing all configuration data, with sections as keys and
             key-value pairs as values.
+
         """
         config_dict = {}
         for section in self.config.sections():
             config_dict[section] = {}
             for key in self.config[section]:
-                config_dict[section][key] = self.get_value(section, key)
+                config_dict[section][key] = self._convert(section, key)
+        
+        torchani_path = importlib.util.find_spec('torchani').submodule_search_locations[0]
+
+        global_config = config_dict['Global']
+
+        assert not (global_config['netlike1x'] and global_config['netlike2x']), "In configuration file, netlike1x and netlike2x cannot both be True"
+
+        if global_config['netlike1x']:
+            global_config['constants'] = os.path.join(torchani_path, 'resources', 'ani-1x_8x', 'rHCNO-5.2R_16-3.5A_a4-8.params')
+            global_config['elements'] = ['H', 'C', 'N', 'O']
+        elif global_config['netlike2x']:
+            global_config['constants'] = os.path.join(torchani_path, 'resources', 'ani-2x_8x', 'rHCNOSFCl-5.1R_16-3.5A_a8-4.params')
+            global_config['elements'] = ['H', 'C', 'N', 'O', 'S', 'F', 'Cl']
+        else:
+            assert 'constants' in global_config and global_config['constants'] is not None, "Constants pathway  not specified in configuration file."
+            assert 'elements' in global_config and global_config['elements'] is not None, "Elements not specified in configuration file."
+
         return config_dict
--- tests/test_config_handler.py
+++ tests/test_config_handler.py
@@ -7,26 +7,26 @@ class TestConfigHandler(unittest.TestCase):
     def setUp(self):
         self.config_handler = ConfigHandler("tests/test_editor.ini")
 
-    def test_get_value(self):
+    def test_convert(self):
         # Check all potential types (boolean, string, list, NoneType, float, int)
         # boolean
-        self.assertEqual(self.config_handler.get_value("Global", "personal"), True)
+        self.assertEqual(self.config_handler._convert("Global", "personal"), True)
         # string
-        self.assertEqual(self.config_handler.get_value("Global", "functional"), "wb97x")
+        self.assertEqual(self.config_handler._convert("Global", "functional"), "wb97x")
         # list
         self.assertEqual(
-            self.config_handler.get_value("Global", "elements"),
+            self.config_handler._convert("Global", "elements"),
             ["H", "C", "N", "O", "S", "F", "Cl"],
         )
         # NoneType
-        self.assertEqual(self.config_handler.get_value("Global", "constants"), None)
+        self.assertEqual(self.config_handler._convert("Global", "constants"), None)
         # float
-        self.assertEqual(self.config_handler.get_value("Trainer", "lr_factor"), 0.7)
+        self.assertEqual(self.config_handler._convert("Trainer", "lr_factor"), 0.7)
         # int
-        self.assertEqual(self.config_handler.get_value("Trainer", "batch_size"), 2048)
+        self.assertEqual(self.config_handler._convert("Trainer", "batch_size"), 2048)
 
     def test_get_all_values(self):
-        all_values = self.config_handler.get_all_values()
+        all_values = self.config_handler.load_config()
         self.assertIn("Global", all_values)
         self.assertIn("Trainer", all_values)
 
